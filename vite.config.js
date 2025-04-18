5594b7df431c0edc863d63b71ba0a45ef27f44c0
Sarvesh <sarveshkuvalekar2207@gmail.com>
2025-04-18 20:36:12 +0530

    changed vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,23 @@
+// import path from "path"
+// import react from "@vitejs/plugin-react"
+// import { defineConfig } from "vite"
+
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// })
+
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Add this line for proper path resolution in production
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
